848e269e568ea30e50d53c207083d8c704e70558
Giulia <giulia.brugnatti@studio.unibo.it>
2021-03-09 10:37:43 +0100

    ex2b completed

--- src/lab_ex/Ex2bFunction.scala
+++ src/lab_ex/Ex2bFunction.scala
@@ -1,16 +1,34 @@
 package lab_ex
-
+/*
+curried and not curried fatte sulla base dell'ex di pag 27, ma non ci ho capito molto
+ */
 object Ex2bFunction {
   //ex4
-  //curriedFunType
-  // p1: =
-  // notcurriedFunType
-  // p2:  =
-  //def p3(..)(..)(..):...=..
-  //def p4
+  //WARM-UP SUM ELEMENTS: RETURN RESULT
+  val simpleNoCurryingFunction:(Int, Int)=>Int=(x,y)=> x+y
+  def simpleNoCurryingMethod(x:Int, y:Int )= simpleNoCurryingFunction(x,y)
+  val simpleCurryingFunction: Int => Int=> Int = x=>y=>simpleNoCurryingFunction(x,y)
+  def simpleCurryingMethod(x:Int)=(y:Int)=> simpleNoCurryingFunction(x,y)
 
-  //ex5: create function that implements functional compositions (fog) (x)=f(g(x))
-  compose(f:Int=>Int, g:Int=>Int): Int =>Int
-  compose(_-1, _*2)(5)
+  //NotcurriedFunType
+  val p1:(Int, Int, Int)=>Boolean = (x,y,z)=> (x<=y)&&(y<=z)
+  // CurriedFunType
+  val p2: Int=>Int=>Int=>Boolean = x=>y=>z=> p1(x,y,z)
+  //notCurried
+  def p3(x:Int,y:Int,z:Int)= p1(x,y,z)
+  //Curried
+  def p4(x:Int) (y:Int) (z:Int): Boolean =p1(x,y,z)
+
+
+  //WARM UP COMPOSITION
+  def simpleComposeMethod (f:Int=>Int, g:Int=>Int, x:Int)=f(g(x))
+  val simpleComposeFunction:(Int=>Int, Int=>Int, Int)=>Int=(f,g,x)=> f(g(x))
+
+  //ex5: create function that implements functional compositions
+  def composeMethod (f:Int=>Int, g:Int=>Int): Int=>Int=x=>f(g(x))
+  //val composeFunction:(Int=>Int, Int=>Int)=>Int=>Int = f=>g=>composeMethod(f, g)(x)
+   //NON CHIARO COSA CI DEVO METTERE DOPO
+  def composeMethodGeneric[A,B,C](f:B=>C, g:A=>B): A=>C= x=> f(g(x))
+
 
 }
