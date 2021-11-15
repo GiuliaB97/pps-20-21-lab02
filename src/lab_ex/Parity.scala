71ef3742b7c74f8329471a7d06781467e12571e1
GiuliaB97 <60556203+GiuliaB97@users.noreply.github.com>
2021-11-15 10:44:24 +0100

    Update Parity.scala

--- src/lab_ex/Parity.scala
+++ src/lab_ex/Parity.scala
@@ -1,25 +1,11 @@
 package lab_ex
 
 object Parity {
-  //WARM-UPEVEN ODD (Integer->Boolean)
-  //def evenTypesMethod(d1: Integer): Boolean = d1 % 2 == 0
-  //println("Even types method " + evenTypesMethod(7))
-
-  //def oddTypesMethod(d1: Integer): Boolean = !evenTypesMethod(d1)
-  //println("Odd types method " + oddTypesMethod(2))
-
-  //val evenTypesFunction = (x: Int) => x % 2 == 0
-  //println("Even types function " + evenTypesFunction(2))
-  //val oddTypesFunction = (x: Int) => !evenTypesFunction(x)
-  //println("Odd types function " + evenTypesFunction(2))
-
-
-  //2A: EVEN-ODD (Integer->String)
-  //val input: Integer = 3;
   val parityFunction: Int => String = {
     case n if n % 2 == 0 => "even"
     case _ => "odd"
   }
+  //val input: Integer = 3;
   //println("Parity function, value: " + input + ", is: " + parityFunction(input))
 
   def parityMethod(n: Int): String = n match {
